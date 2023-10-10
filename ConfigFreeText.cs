23efe022092ff423f742b90db1bf8abf66134f50
Joe Mucchiello <jmucchiello@gmail.com>
2023-10-10 18:08:26 -0400

    Update ConfigFreeText.cs

--- ConfigFreeText.cs
+++ ConfigFreeText.cs
@@ -1,41 +1,47 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using Shapes;
+using UnityEngine;
 
-namespace EnemyDifficultyModNS
+namespace CommonModNS
 {
     public class ConfigFreeText : ConfigEntryHelper
     {
-        public string Text;
         public TextAlign TextAlign = TextAlign.Left;
+        public string Text { get => Button?.TextMeshPro.text; set { if (Button != null) Button.TextMeshPro.text = AlignText(TextAlign, value); } }
+        public string Tooltip{ get => Button?.TooltipText; set { if (Button != null) Button.TooltipText = value; } }
         public override object BoxedValue { get => new object(); set => _ = value; }
 
         public Action<ConfigEntryBase, CustomButton> Clicked;
+        public Action<ConfigFreeText, CustomButton> OnUI;
+
+        private CustomButton Button;
 
         /**
-         *  Create a header line in the config screen. Also useful for stuff like "Close" in ModalScreen
+         *  Create a header line in the config screen. Also useful for stuff like "Close" or "Reset Defaults"
          **/
         public ConfigFreeText(string name, ConfigFile config, string text, string tooltip = null)
         {
             Name = name;
             Config = config;
             ValueType = typeof(object);
-            Text = text;
             UI = new ConfigUI()
             {
+                NameTerm = text,
+                TooltipTerm = tooltip,
                 Hidden = true,
                 OnUI = delegate (ConfigEntryBase c)
                 {
-                    CustomButton btn = UnityEngine.Object.Instantiate(I.PFM.ButtonPrefab, I.MOS.ButtonsParent);
-                    btn.transform.localScale = Vector3.one;
-                    btn.transform.localPosition = Vector3.zero;
-                    btn.transform.localRotation = Quaternion.identity;
-                    btn.TextMeshPro.text = AlignText(TextAlign, I.Xlat(Text) ?? Text);
-                    btn.TooltipText = tooltip == null ? null : I.Xlat(tooltip) ?? tooltip;
-                    btn.EnableUnderline = false;
-                    btn.Clicked += delegate ()
+                    Button = UnityEngine.Object.Instantiate(I.PFM.ButtonPrefab, I.MOS.ButtonsParent);
+                    Button.transform.localScale = Vector3.one;
+                    Button.transform.localPosition = Vector3.zero;
+                    Button.transform.localRotation = Quaternion.identity;
+                    Button.TextMeshPro.text = RightAlign(I.Xlat(UI.NameTerm));
+                    Button.TooltipText = I.Xlat(UI.TooltipTerm);
+                    Button.EnableUnderline = false;
+                    Button.Clicked += delegate ()
                     {
-                        Clicked?.Invoke(this, btn);
+                        Clicked?.Invoke(this, Button);
                     };
+                    OnUI?.Invoke(this, Button);
                 }
             };
             config.Entries.Add(this);
